45c4b0f19a43fb3c824e806489968fd90219e989
ethelegend <michel.elijah.k@gmail.com>
2024-12-03 21:38:10 +1300

    Challenge 3

--- src/main/java/challenges/Challenges.java
+++ src/main/java/challenges/Challenges.java
@@ -7,6 +7,8 @@ public class Challenges {
                 new HistorianHysteria();
             case 2:
                 new RedNosedReports();
+            case 3:
+                new MullItOver();
         }
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Challenges.runChallenge(2);
+        Challenges.runChallenge(3);
     }
 
     public static int[] nextInts() {
